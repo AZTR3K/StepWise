1c13c7d005a76332aa4d1f3b7a7d33f768ba5675
BabaYaga7 <m41377040@gmail.com>
2026-02-27 12:50:12 +0500

    Initial Commit

---
+++ StepWiseTests/StepWiseTests.swift
@@ -0,0 +1,16 @@
+//
+//  StepWiseTests.swift
+//  StepWiseTests
+//
+//  Created by Mohammad on 27/02/2026.
+//
+
+import Testing
+
+struct StepWiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
